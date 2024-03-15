01732cedbf08e9c517ae28a28fe111a331bfa1a0
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-03-15 14:16:31 -0700

    Update predictor.cc

--- predictor.cc
+++ predictor.cc
@@ -1,67 +1,68 @@
-/* Author: Sai Kumar, Reshwanth;                                                                                                                                                                                             
- * Description: This file defines the two required functions for the branch predictor.                                                                                                                             
-*/
-
 #include "predictor.h"
 
-int local_history[1024];
-int local_prediction[1024];
-int global_prediction[4096];
-int choice_prediction[4096];
-int path_history;
+uint local_history[1024];
+uint local_prediction[1024];
+uint global_prediction[4096];
+uint choice_prediction[4096];
+uint path_history;
 
+//initialization
+void initialization(){
+
+	for(uint i=0; i<1024;i++)
+	{
+		local_history[i]=0;
+	}
+	for(uint j=0; j<1024; j++)
+	{
+		local_prediction[j]=7;
+	}
+	for(uint k=0; k<4096; k++)
+	{
+		global_prediction[k]=3;
+	}
+	for(uint l=0; l<4096; l++)
+	{
+		choice_prediction[l]=3;
+	}
+	path_history=0;
+	return;
+}
 
 #define  global_mask  0xFFF
 #define  local_mask  0x3FF
 #define  pc_mask  0xFFC
  
-int global_histbits;
-int choice;
-int pc_idx;
-int local_hist;
+uint global_idx;
+uint choice;
+uint pc_idx;
+uint local_idx;
+
 bool local_pred;
 bool global_pred;
-int l_pred;
-int g_pred;
          
 bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os)
-       {
-		 
-		pc_idx = (pc_mask & br->instruction_addr)>>2;
-        local_hist = local_mask & local_history[pc_idx];		   
-	    global_histbits = global_mask & path_history;
-        choice = choice_prediction[global_histbits];
-	    g_pred=global_prediction[global_histbits];
-		l_pred=local_prediction[local_hist];
-       
+{
+	pc_idx = (pc_mask & br->instruction_addr)>>2;
+    local_idx = local_mask & local_history[pc_idx];		   
+	global_idx = global_mask & path_history;
+	
     if(br->is_conditional)
-    {
-		   
-        if(choice<2)
-        {
-        
-            if(l_pred>3){
+    {   if(choice_prediction[global_idx]>1)
+		{    if(global_prediction[global_idx]>1)
 	            return true;
-		    }
-		    else{
+		    else
+				return false;
+		}
+		else{
+            if(local_prediction[local_idx]>3)
+	            return true;
+		    else
 		    	return false;
-		    }
-        }
-        else
-        {
-            if(g_pred>1)
-		    {
-	            return true;
-		    }
-		    else{
-	            return false;
-		    }
-        }
+		}
     }
-    else{
-	    return true;
-    }
-                                                                                                                                       
+    else
+	   return true;
 }
 
                                                                                                                                             
@@ -69,63 +70,49 @@ void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os
 {
      
     pc_idx = (pc_mask & br->instruction_addr)>>2;
-    local_hist = local_mask & local_history[pc_idx];                   
-
+    local_idx = local_mask & local_history[pc_idx];                   
+    global_idx = global_mask & path_history;
+	
 	if(br->is_conditional){	              
-        global_histbits = global_mask & path_history;
-        choice = choice_prediction[global_histbits];
-		g_pred=global_prediction[global_histbits];
-		l_pred=local_prediction[local_hist];
-        	  
-      
-     // if(choice_prediction[global_histbits]<2){
-            if(l_pred>3)
+            if(local_prediction[local_idx]>3)
                 local_pred = true;
             else
                 local_pred = false;
-      //}
-     // else {
-            if(g_pred>1)
+
+            if(global_prediction[global_idx]>1)
                 global_pred= true;
             else
                 global_pred = false;
-      //}
-	    
 
-        if(global_pred==taken && local_pred!=taken && choice_prediction[global_histbits]!=3)
-            choice_prediction[global_histbits]++;
-        else if(global_pred!=taken && local_pred==taken && choice_prediction[global_histbits]!=0)
-            choice_prediction[global_histbits]--;
-
-
+        if(global_pred==taken && local_pred!=taken && choice_prediction[global_idx]!=3)
+            choice_prediction[global_idx]++;
+        else if(global_pred!=taken && local_pred==taken && choice_prediction[global_idx]!=0)
+            choice_prediction[global_idx]--;
+		else
+			choice_prediction[global_idx]=choice_prediction[global_idx];
+		
         if(taken)
         {
-            if(global_prediction[global_histbits]!=3)
-                global_prediction[global_histbits]++;
+            if(global_prediction[global_idx]!=3)
+                global_prediction[global_idx]++;
 		
-            if(local_prediction[local_hist]!=7)
-                local_prediction[local_hist]++;
+            if(local_prediction[local_idx]!=7)
+                local_prediction[local_idx]++;
         }
         else
         {
-            if(global_prediction[global_histbits]!=0)
-                global_prediction[global_histbits]--;
+            if(global_prediction[global_idx]!=0)
+                global_prediction[global_idx]--;
 	 
-            if(local_prediction[local_hist]!=0)
-                local_prediction[local_hist]--;
+            if(local_prediction[local_idx]!=0)
+                local_prediction[local_idx]--;
         }
 	
         local_history[pc_idx] = ((local_history[pc_idx]<<1) | taken) & local_mask;
         path_history = ((path_history<<1) | taken) & global_mask;
     }
 	else
-	{
 		local_history[pc_idx] = ((local_history[pc_idx]<<1) | taken) & local_mask;
-		 
-	}
 
 	return;				  
-
 }
-
-
