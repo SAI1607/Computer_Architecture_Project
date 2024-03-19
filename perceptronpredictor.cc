2e78dbfc5ed82d78f6ecdbb7e4f6159d072e2782
SAI KUMAR REDDY PULAGAM <52541991+SAI1607@users.noreply.github.com>
2024-03-19 12:55:18 -0700

    Update perceptronpredictor.cc

--- perceptronpredictor.cc
+++ perceptronpredictor.cc
@@ -1,24 +1,17 @@
-// A Perceptron Based predictor.
-
+//Perceptron Based Predictor//
 #include "predictor.h"
 
 // Number of weights
 #define N 28
 
-
-// Total budget of the predictor
-#define Budget (32800)
-
 // Threshold values
 #define ThetaMax (N*1.93+14)
 #define ThetaMin ( -1*ThetaMax )
 
-// Number of bits in a weight
 #define BitsInWeight 8
 #define MAXVAL 127
 #define MINVAL -128
 
-// calculation of entries of perceptron table
 #define SizeOfPerceptron ((N+1)*BitsInWeight)
 #define NumberPerceptron 128
 
@@ -27,41 +20,34 @@ int bias[NumberPerceptron];
 int GHR;
 int indexxx;
 int y;
+int hist;
+int idx;
 bool prediction;
 
 void init_predictor ()
 {
-  int i,j;
-  for(i=0; i<NumberPerceptron; i++)
-	for (j=0 ; j <= N ; j++)
+  for(int i=0; i<NumberPerceptron; i++)
+	for (int j=0 ; j <= N ; j++)
 		perceptron[i][j] = 0;
-  GHR = 0; 
+  GHR = 0;
 }
 
 bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os)
 {
-  int i,j,x;
-  prediction = false;
-
-  // hash function to map pc to a row in the perceptron table
+  int j,x;
   indexxx = br->instruction_addr % NumberPerceptron;
   j = 1;
   y = bias[indexxx];
-  for(i=0; i < N; i++){
-	  // checking jth bit of GHR 
+  for(int i=0; i < N; i++){
 	  if((GHR&j) == 0) 
 		x = -1;
 	  else
 		x = 1;
-
-	  // dot product calculation
+	  
 	  y += perceptron[indexxx][i]*x;
 
-	  // used in finding out jth bit of GHR above
 	  j = j << 1;
 	}
-
-  // making a prediction
   if( y >= 0 )
 	prediction = true;
   else
@@ -72,48 +58,30 @@ bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os)
 
 void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os, bool taken)
 {
- int i,j,x,t;
-
- // t used to update bias and numerical outcome is used to update GHR
+ int j,x,t;
  if(taken){
 	t = 1;
  }
  else{
 	t = -1;
  }
- 
  j = 1;
 
- // updating weights and bias
  if( taken != prediction || (y < ThetaMax && y > ThetaMin) ){
-
-	// checking whether bias is within a value that is 8 bit long
 	if ( bias[indexxx] > MINVAL && bias[indexxx] < MAXVAL) {
-		// updating bias
 		bias[indexxx] = bias[indexxx] + t;
 	}
-
-	for(i=0; i < N; i++){	
-
-		// checking whether correlation is positive or negative
+	for(int i=0; i < N; i++){	
 		if( ( ( (GHR&j) != 0) && taken ) ||  ( ((GHR&j) == 0) && !taken ) ) 
 			x = 1;
 		else
 			x = -1;
-
-		// checking whether ith weight at index is within a value that is 8 bit long
 		if ( perceptron[indexxx][i] > MINVAL && perceptron[indexxx][i] < MAXVAL ) {
-			// updating weight 
 			perceptron[indexxx][i] = perceptron[indexxx][i] + x;
 		}
-
-		// j used in finding out jth bit of GHR
 		j = j << 1;
 	}
 
  }
- 
- // updating .. we take care of number of bits in GHR by doing an & with j everywhere we use GHR
- // in any case since it is an int it is just 32 bits well under 256 bits reserved for GHR
  GHR = GHR << 1 | taken;
 }
